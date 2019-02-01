1d750d07aea9e3e9a1357a9dbe757557699b0a81
Alcat101 <adhanika15@gmail.com>
2019-02-01 12:27:40 +0700

    Ammo Belt asset, Bipod model, CAP value
    Added prototype ammo belt asset
    3-4 separate ksp58 mag standard linked together with lua 'adds' function
    Uses universal material_config (normal, and thq), lua code on wpnfctrytwkdata
    CAP used heavily to reposition
    
    Added Bipod model
    note that it only works if user have Improved Bipod model
    vanilla bipod model will be used if user doesnt use the mod
    code on wpnfctrytwkdata
    
    Localization changes

--- Hooks/weaponfactorytweakdata.lua
+++ Hooks/weaponfactorytweakdata.lua
@@ -2,15 +2,26 @@ Hooks:PostHook( WeaponFactoryTweakData, "init", "SumitomoModInit", function(self
 	
 
 --check compatibility with mod that alter bipod, otherwise, use vanilla bipod arrangement
--- if BeardLib.Utils:FindMod("Improved Bipods") then
-	-- self.wpn_fps_lmg_sumitomo.override = {
-		-- wpn_fps_upg_bp_lmg_lionbipod = { unit = "units/mods/weapons/wpn_fps_lmg_sumitomo_bipod/wpn_fps_lmg_sumitomo_bipod_improved" }
-	-- }
--- else
-	-- self.wpn_fps_lmg_sumitomo.override = {
-		-- wpn_fps_upg_bp_lmg_lionbipod = { unit = "units/mods/weapons/wpn_fps_lmg_sumitomo_bipod/wpn_fps_lmg_sumitomo_bipod_vanilla" }
-	-- }
--- end
+if BeardLib.Utils:FindMod("Improved Bipods") then
+	self.wpn_fps_lmg_sumitomo.override = {
+		wpn_fps_upg_bp_lmg_lionbipod = { unit = "units/mods/weapons/wpn_fps_lmg_sumitomo_bipod/wpn_fps_lmg_sumitomo_bipod_improved" }
+	}
+else
+	self.wpn_fps_lmg_sumitomo.override = {
+		wpn_fps_upg_bp_lmg_lionbipod = { unit = "units/pd2_crimefest_2015/update_5/weapons/wpn_fps_upg_bp_lmg_lionbipod/wpn_fps_upg_bp_lmg_lionbipod" }
+	}
+end
+
+
+--ammo belt stuff
+self.parts.wpn_fps_lmg_sumitomo_m_belt.thq_material_config = Idstring("units/mods/weapons/wpn_fps_lmg_sumitomo_ammobelt/wpn_fps_lmg_sumitomo_ammo_universal_thq")
+self.parts.wpn_fps_lmg_sumitomo_m_belt2.thq_material_config = Idstring("units/mods/weapons/wpn_fps_lmg_sumitomo_ammobelt/wpn_fps_lmg_sumitomo_ammo_universal_thq")
+self.parts.wpn_fps_lmg_sumitomo_m_belt3.thq_material_config = Idstring("units/mods/weapons/wpn_fps_lmg_sumitomo_ammobelt/wpn_fps_lmg_sumitomo_ammo_universal_thq")
+self.parts.wpn_fps_lmg_sumitomo_m_belt4.thq_material_config = Idstring("units/mods/weapons/wpn_fps_lmg_sumitomo_ammobelt/wpn_fps_lmg_sumitomo_ammo_universal_thq")
+
+-- self.parts.wpn_fps_lmg_sumitomo_m_belt2.bullet_objects = { amount = 8, prefix = "g_bullet_" }
+-- self.parts.wpn_fps_lmg_sumitomo_m_belt3.bullet_objects = { amount = 11, prefix = "g_bullet_" }
+-- self.parts.wpn_fps_lmg_sumitomo_m_belt4.bullet_objects = { amount = 14, prefix = "g_bullet_" }
 
 
 end )
--- Hooks/weapontweakdata.lua
+++ Hooks/weapontweakdata.lua
@@ -1,5 +1,47 @@
 Hooks:PostHook(WeaponTweakData, "init", "SumitomoModInit", function(self)
 
-
+if self.sumitomo then
+	self.sumitomo.attachment_points = {
+		--Barrel Repositioning
+		{
+			name = "a_b",
+			base_a_obj = "a_b",
+			position = Vector3(0, 0, 0),
+		},
+		
+		--Bipod Position
+		{
+			name = "a_bp",
+			base_a_obj = "a_bp",
+			position = Vector3(0, 20, 0),
+			rotation = Rotation(0, 0, 0)
+		},
+		
+		--Ammo belt Positioning
+		{
+			name = "a_m2",
+			base_a_obj = "a_m",
+			position = Vector3(-0.25, 0, -4.5),
+		},
+		{
+			name = "a_m3",
+			base_a_obj = "a_m",
+			position = Vector3(-0.25, 0, -9),
+		},
+		{
+			name = "a_m4",
+			base_a_obj = "a_m",
+			position = Vector3(-0.25, 0, -13.5),
+		},
+		
+		--Gadget Position
+		{
+			name = "a_fl",
+			base_a_obj = "a_fl",
+			position = Vector3(0, 0, 0),
+			rotation = Rotation(0, 0, 0)
+		}
+	}
+end
 
 end)
